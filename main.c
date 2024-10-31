394f365a37f56fd0bd6c94602bf17a8228d7afb6
Miraç Furkan Durmaz <117925817+mirac2534@users.noreply.github.com>
2024-10-31 17:29:58 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,12 +4,12 @@ void reportTheWeather(float,float);
 void  fromcelciustokelvin(float,float);
 int main()
 {
-float celcius, humidity, kelvin;
+float celcius, humidity, kelvin; // Defined celcius, kelvin, and humudity
 printf("Welcome to the situation of the weather \nEnter celcius:");
-scanf("%f",&celcius);
+scanf("%f",&celcius); // Enter celcius
 printf("Enter humidity level(0-100):");
-scanf("%f",&humidity);
-fromcelciustokelvin(celcius,humidity);
+scanf("%f",&humidity); // Enter humudity
+fromcelciustokelvin(celcius,humidity); // Change from celcius to kelvin
 
     return 0;
 }
@@ -17,27 +17,27 @@ fromcelciustokelvin(celcius,humidity);
 void  fromcelciustokelvin(float celcius,float humidity)
 {
 const float x=273;
-    float kelvin=celcius+x;
+    float kelvin=celcius+x; // Calculates kelvin with celcius
 return reportTheWeather(kelvin,humidity);
 }
 
 void reportTheWeather(float kelvin,float humidity )
  {
-if (kelvin>298.0 && humidity<70.0 )
+if (kelvin>298.0 && humidity<70.0 ) // If kelvin is bigger than 298 and humudity is smaller than 70
 {
 printf("Situation of the weather is sunny\n");
 }
- else if (kelvin<283.0 )
+ else if (kelvin<283.0 ) // If kelvin is smaller than 283
  {
 printf("Situation of the weather is cold");
  }
- else if (kelvin>=283.0 && kelvin<=298.0)
+ else if (kelvin>=283.0 && kelvin<=298.0) // If kelvin is between 298 and 283 
  {
-    if (humidity>80.0)
+    if (humidity>80.0) // And if humudity is bigger than 80
  {
  printf("Situation of the weather is rainy");
  }
-    else if (humidity>70.0 && humidity<=80.00)
+    else if (humidity>70.0 && humidity<=80.00) // Also if humudity is between 70 and 80
  {
 printf("Situation of the weather is cloudy");
  }
